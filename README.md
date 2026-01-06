2b25657bdbc28e079df0da6247314e5b056864af
Evelyn Warton <me@evelynw.xyz>
2026-01-06 14:43:05 -0800

    Added .gitignore

---
+++ README.md
@@ -0,0 +1,18 @@
+# SFLA (Simple Fast Linear Algebra)
+
+My personal C library for doing ocmputational linear algebra and matrix math.
+
+## Immediate TODO:
+- Represent with row major and vectors
+- Row / column vector access
+
+## Far-off TODO:
+- Multiplicaiton
+- Determinant
+- LU Decomposition
+- Inversion
+- Psuedoinverse
+- SVD
+- Eigenvectors
+- Eigenvalues
+- Diagonalization
---
+++ main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef struct {
+	int rows;
+	int cols;
+	double* data;
+} mtrx;
+
+mtrx* mtrx_alloc(int rows, int cols) {
+	mtrx* M = malloc(sizeof(mtrx));
+	M->rows = rows;
+	M->cols = cols;
+	M->data = malloc(rows * cols * sizeof(double));
+	return M;
+}
+
+void mtrx_free(mtrx* M) {
+	free(M->data);
+	free(M);
+}
+
+void mtrx_fill(mtrx* M, double value) {
+	for (int i = 0; i < M->rows; i++) {
+		for (int j = 0; j < M->cols; j++) {
+			M->data[i * M->cols + j] = value;
+		}
+	}
+}
+
+mtrx* mtrx_zeros(int rows, int cols) {
+	mtrx* M = mtrx_alloc(rows, cols);
+	mtrx_fill(M, 0.0);
+	return M;
+}
+
+mtrx* mtrx_ones(int rows, int cols) {
+	mtrx* M = mtrx_alloc(rows, cols);
+	mtrx_fill(M, 1.0);
+	return M;
+}
+
+mtrx* mtrx_id(int rows, int cols) {
+	assert(rows == cols);
+	mtrx* M = mtrx_alloc(rows, cols);
+	for (int i = 0; i < rows; i++) {
+		M->data[i * rows + i] = 1.0;
+	}
+	return M;
+}
+
+void mtrx_print(const mtrx *M) {
+	for (int i = 0; i < M->rows; i++) {
+		for (int j = 0; j < M->cols; j++) {
+			printf("%f ", M->data[i * M->cols + j]);
+		}
+		printf("\n");
+	}
+}
+
+void mtrx_set_elem(mtrx* M, int row, int col, double value) {
+	// Check index validity
+	assert(row < M->rows);
+	assert(col < M->cols);
+
+	M->data[row * M->cols + col] = value;
+}
+
+double mtrx_get_elem(mtrx* M, int row, int col) {
+	// Check index validity
+	assert(row < M->rows);
+	assert(col < M->cols);
+	
+	return M->data[row * M->cols + col];
+}
+
+void mtrx_swp_rows(mtrx* M, int row_a, int row_b) {
+	assert(row_a < M->rows);
+	assert(row_b < M->rows);
+	for (int i = 0; i < M->cols; i++) {
+		double tmp = M->data[row_a * M->cols + i];
+		M->data[row_a * M->cols + i] = M->data[row_b * M->cols + i];
+		M->data[row_b * M->cols + i] = tmp;
+	}
+}
+
+void mtrx_transpose(mtrx* M) {
+	for (int i = 0; i < M->rows; i++) {
+		for (int j = 0; j < M->cols; j++) {
+			double tmp = M->data[i * M->cols + j];
+			M->data[i * M->cols + j] = M->data[j * M->cols + i];
+			M->data[j * M->cols + i] = tmp;
+		}
+	}
+}
+
+void mtrx_mem_test() {
+	mtrx* M = mtrx_id(4, 4);
+	mtrx_swp_rows(M, 1, 2);
+	mtrx_print(M);
+	mtrx_free(M);
+}
+
+int main() {
+	mtrx_mem_test();
+	return 0;
+}
+
